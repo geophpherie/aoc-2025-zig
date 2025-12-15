3f4459e14079c90ad2765f9d1b3ab6ef4b80c577
Jeff Langdon <jeff.beyer@gmail.com>
2025-12-14 22:35:07 -0800

    day 7

--- day_7/part2.zig
+++ day_7/part2.zig
@@ -17,7 +17,10 @@ pub fn main() !void {
 
     const result = try puzzle(allocator, input);
 
-    std.debug.print("Part 1 answer : {d}\n", .{result});
+    std.debug.print("Part 2 answer : {d}\n", .{result});
+    // 3084 too low
+    // 10000 too low
+    // 3223365367809
     std.debug.print("Elapsed time: {d} ms\n", .{timer.read() / 1000 / 1000});
 }
 
@@ -26,38 +29,67 @@ fn puzzle(allocator: std.mem.Allocator, input: []const u8) !u64 {
     var splitLines = std.mem.splitScalar(u8, trimmed, '\n');
 
     // keep each line as an array of chars
-    var lines = std.ArrayList([]u8){};
+    var lines = std.ArrayList([]u8).empty;
     while (splitLines.next()) |line| {
         if (line.len == 0) continue;
 
         const mutArray = try allocator.dupe(u8, line);
         try lines.append(allocator, mutArray);
     }
-    var grid = try lines.toOwnedSlice(allocator);
+    const grid = try lines.toOwnedSlice(allocator);
 
-    var numSplits: u64 = 0;
-    for (grid[1..grid.len], 1..) |line, row| {
-        for (line, 0..) |char, col| {
-            // std.debug.print("checking: {c}\n", .{lines.items[row-1][col]});
-            if (grid[row - 1][col] == 'S') {
-                // std.debug.print("starting beam above : {c}\n", .{char});
-                grid[row][col] = '|';
-            } else if (char == '^' and grid[row - 1][col] == '|') {
+    const gridLength = grid.len;
+
+    // var numTimelines: u64 = 0;
+
+    var prevRowCounts = try allocator.alloc(u64, grid[0].len);
+    @memset(prevRowCounts, 0);
+
+    for (1..gridLength) |r| {
+        for (grid[r], 0..) |char, col| {
+            if (char == '^' and grid[r - 1][col] == '|') {
                 // this is a split
-                numSplits += 1;
-                grid[row][col - 1] = '|';
-                grid[row][col + 1] = '|';
-            } else if (grid[row - 1][col] == '|') {
-                grid[row][col] = '|';
+                grid[r][col - 1] = '|';
+                grid[r][col + 1] = '|';
+
+                // count the num of beams in each course
+                prevRowCounts[col - 1] += prevRowCounts[col];
+                prevRowCounts[col + 1] += prevRowCounts[col];
+
+                // this col dies here then
+                prevRowCounts[col] = 0;
+            } else if (grid[r - 1][col] == 'S') {
+                prevRowCounts[col] += 1;
+                grid[r][col] = '|';
+            } else if (grid[r - 1][col] == '|') {
+                grid[r][col] = '|';
             }
         }
+        // std.debug.print("line: {s} __{d}^ __{d} timelines\n", .{ grid[r], numCarr, numTimelines });
+        std.debug.print("line: {s} -- {d}\n", .{ grid[r], r });
+        std.debug.print("counts: {any}\n", .{prevRowCounts});
+    }
+    // 2, 4, 8, 12, 17, 20, 25
+    // total :  1, 2, 4, 8, 13
+
+    var sum: u64 = 0;
+    for (prevRowCounts) |item| {
+        sum += item;
+    }
+    return sum;
+}
+
+fn dupeGrid(
+    allocator: std.mem.Allocator,
+    grid: [][]u8,
+) ![][]u8 {
+    var copy = try allocator.alloc([]u8, grid.len);
+
+    for (grid, 0..) |row, i| {
+        copy[i] = try allocator.dupe(u8, row);
     }
 
-    for (grid) |line| {
-        std.debug.print("line: {s}\n", .{line});
-    }
-
-    return numSplits;
+    return copy;
 }
 
 fn readFile(allocator: std.mem.Allocator, filename: []const u8) ![]u8 {
@@ -98,5 +130,5 @@ test "test input" {
 
     const result = try puzzle(allocator, input);
 
-    try std.testing.expectEqual(21, result);
+    try std.testing.expectEqual(40, result);
 }
